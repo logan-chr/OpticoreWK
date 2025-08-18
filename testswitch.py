9138b1b7645a0b3512b96fb37f927404274bfd38
Logan <tmp_logan.christensen@opticoreit.com>
2025-08-18 15:17:13 +0100

    commit

--- testswitch.py
+++ testswitch.py
@@ -1,5 +1,7 @@
 import netmiko
 from netmiko import ConnectHandler
+import pandas as pd
+
 switch = {
     'device_type':'cisco_ios',
     'host':'10.1.10.240',
@@ -21,13 +23,34 @@ def noshutdown():
     arr = ['configure terminal','interface fastEthernet 0/3','no shutdown','exit','logout']
     ssh.send_config_set(arr)
 def peek():
+    print('peeking')
     ssh = ConnectHandler(**switch)
     ssh.enable()
-    arr = ['configure terminal','interface fastEthernet 0/3','show running-config interface fastEthernet 0/3','exit','logout']
-    ssh.send_config_set(arr)
-    
+    output = ssh.send_command('show running-config interface fastEthernet 0/3')
+    if 'shutdown' in output:
+        print('off')
+    else:
+        print('on')
+    ssh.disconnect
 
-while True:
+
+def power():
+    print('peeking')
+    ssh = ConnectHandler(**switch)
+    ssh.enable()
+    output = ssh.send_command('show power inline')
+    frame = pd.DataFrame(output)
+    print(frame)
+
+    
+    ssh.disconnect
+
+
+flag = True
+while flag:
+    print()
+    print('======')
+    print('>>>',end='')
     a = input()
     if a == 'on':
         noshutdown()
@@ -35,4 +58,8 @@ while True:
         shutdown()
     elif a=='peek':
         peek()
+    elif a=='power':
+        power()
+    elif a=='end':
+        flag = False
     
