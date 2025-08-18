92760ec3764c3874b46440ed2135b9dc2f6c6435
Logan <tmp_logan.christensen@opticoreit.com>
2025-08-18 16:34:15 +0100

    sensor now working

--- sensor.py
+++ sensor.py
@@ -3,18 +3,12 @@ import time
 
 
 GPIO.setmode(GPIO.BCM)
-PIR_PIN = 2  # Replace with the GPIO pin connected to the sensor
+PIR_PIN = 4  # Replace with the GPIO pin connected to the sensor
 GPIO.setup(PIR_PIN, GPIO.IN)
-
-print("Waiting for motion...")
-try:
-    while True:
-        if GPIO.input(PIR_PIN):
-            print("Motion detected!")
-        time.sleep(1)
-        
-except KeyboardInterrupt:
-    print("Exiting...")
-finally:
-    GPIO.cleanup()
+n = 0
+while True:
+    if GPIO.input(PIR_PIN):
+        n+= 1
+        print(n)
+    time.sleep(1)
 
