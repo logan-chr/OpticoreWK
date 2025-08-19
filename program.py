36757f8e98bf11c0340dcecfaf82857b1b2da0d7
Logan <tmp_logan.christensen@opticoreit.com>
2025-08-19 09:26:54 +0100

    storing uptime in file

--- program.py
+++ program.py
@@ -2,22 +2,31 @@ from testswitch import switch, noshutdown,shutdown,peek,power
 from sensor import sense
 import time
 noshutdown()
-timeout = 30
+MAXTIME = 10
+timeout = MAXTIME
+uptime = 0
 while True:
+    uptime += 1
     print(timeout)
     if timeout > 0:
         timeout -=1
     time.sleep(1)
     if sense():
-        timeout = 30
+        timeout = MAXTIME
         if not peek():
             noshutdown()
             print('power turned on')
     if timeout <= 0:
         shutdown()
         print('timeout, power turned off')
+        # Writing to a file
+        with open('file.txt', 'a') as file:
+            file.write(str(str(uptime)+' '))
+        uptime = 0
         while not sense():
             pass
+        timeout = MAXTIME
+
 
 
 
